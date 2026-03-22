53ecb4f905515404da36d02523036df6f135ea8b
maycon <maynions@gmail.com>
2026-03-22 15:53:19 -0300

    versão 7

---
+++ kubejs/startup_scripts/itens.js
@@ -0,0 +1,20 @@
+StartupEvents.registry('item', event => {
+event.create('invertnite')
+event.create('realinite')
+event.create('dimensionite')
+event.create('realinite_nugget')
+event.create('invertinete_nugget')
+event.create('dimensionite_nugget')
+event.create('pomagico')
+event.create('advanced_engine')
+event.create('supreme_advanced_magic')
+event.create('supreme_advanced_engine')
+event.create('supreme_advanced_alchemy_of_machines')
+event.create('star')
+event.create('singularity')
+    } 
+  );
+
+
+ 
+ 
