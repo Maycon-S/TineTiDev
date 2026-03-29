4a945a54a72308154b0318f2c18c0e681312c576
maycon <maynions@gmail.com>
2026-03-29 12:02:51 -0300

    versão 7.41
    teste

--- kubejs/startup_scripts/block.js
+++ kubejs/startup_scripts/block.js
@@ -26,3 +26,41 @@ StartupEvents.registry('block', event => {
     .tagBlock('minecraft:mineable/pickaxe')
     .tagBlock('minecraft:needs_diamond_tool');
 });
+
+StartupEvents.registry('block', event => [
+event.create('machine_block').soundType('netherite_block').blockEntity (entityInfo => { // entityInfo é o objeto que representa a entidade do bloco, permitindo definir seu comportamento e características.
+entityInfo.inventory (9, 6) // Define um inventário com 9 slots e 4 linhas para a entidade do bloco, permitindo armazenar itens dentro do bloco.
+entityInfo.rightClickOpensInventory() // Permite que os jogadores abram o inventário do bloco ao clicar com o botão direito do mouse, facilitando a interação com o bloco.
+entityInfo.clientTick(20, 0, entity => { // Define um comportamento que ocorre a cada 20 ticks (1 segundo) no lado do cliente, permitindo criar efeitos visuais ou interações específicas.
+entity.level.addParticle('minecraft:campfire_cosy_smoke', true, entity.x + 0.5, entity.y + 1.05, entity.z + 0.5, 0, 0.3, 0) // Adiciona partículas de fumaça aconchegante de fogueira ao redor do bloco, criando um efeito visual interessante.
+})
+entityInfo.serverTick (20, 0, entity => { // Define um comportamento que ocorre a cada 20 ticks (1 segundo) no lado do servidor, permitindo criar lógicas de jogo ou interações específicas.
+entity.inventory.insertItem('minecraft:apple', false)  // Insere uma maçã no inventário do bloco, permitindo que o bloco armazene itens e interaja com eles de maneira dinâmica.
+})
+  })
+]); 
+
+StartupEvents.registry('block', event => {
+  event.create('teste').blockEntity (entityInfo => {
+    entityInfo.inventory (9, 6)
+    entityInfo.rightClickOpensInventory()
+    })
+  });
+
+  StartupEvents.registry('block', event => {
+  event.create('meu_bloco_customizado')
+    .blockEntity(builder => {
+      // Executa código a cada tick (20 vezes por segundo)
+      builder.ticker((level, pos, state, be) => {
+        if (!level.clientSide && level.time % 20 == 0) {
+            console.info("Estou tickando no servidor!");
+        }
+      })
+      // Adiciona um inventário com 9 slots
+      .itemHandler(9)
+      // Define valores padrões para dados NBT persistentes
+      .defaultValues(tag => {
+        tag.progress = 0
+      })
+    })
+})
